7e169e68568956caca80b0cccf07d15807973ba8
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-05-18 10:55:04 -0400

    adicionado cores custom ao tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className="text-2xl font-bold uppercase">oi</div>;
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50">
+      <div className="text-2xl font-bold uppercase">Hello World</div>
+      <div className="font-alt text-2xl font-bold uppercase">Hello World</div>
+    </div>
+  );
 }
